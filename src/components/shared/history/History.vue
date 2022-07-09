ebe09407937680f1aaa2b048ca898a398abb3615
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-07-09 12:21:36 -0300

    Ajustes finais

--- src/components/shared/history/History.vue
+++ src/components/shared/history/History.vue
@@ -100,7 +100,6 @@ export default {
                     this.historyPrice = '';
                     this.historyDescription = '';
                 })
-            console.log(history)
         }
     },
     computed: {
